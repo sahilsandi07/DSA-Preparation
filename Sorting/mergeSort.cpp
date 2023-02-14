8904d6491bd056c8ef0fb92ac13898705b7cacdc
Sahil Sandi <officialsahilsandi@gmail.com>
2023-02-14 10:44:22 +0530

    Merge Sort

--- Sorting/mergeSort.cpp
+++ Sorting/mergeSort.cpp
@@ -1,35 +1,88 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-void merge(int a[], int b[], int m, int n){
-    int i=0, j=0;
+void merge(int *array, int start_ind, int end_ind){
 
-    while (i<m && j<n){
-        if (a[i] <= b[j]){
-            cout << a[i++] <<" ";
+    int mid_ind = (start_ind + end_ind)/2;
+
+    //find length 
+    int len1 = mid_ind - start_ind + 1;
+    int len2 = end_ind - mid_ind;
+
+    //create two arrays of length len1 and len2
+    int *first = new int [len1];
+    int *second = new int [len2];
+
+    //copy values
+    int maxArrayIndex = start_ind;
+
+    for (int i=0; i<len1; i++){
+        first[i] = array[maxArrayIndex++];
+    }
+
+    maxArrayIndex = mid_ind + 1;
+
+    for (int i=0; i<len2; i++){
+        second[i] = array[maxArrayIndex++];
+    }
+
+    // Merge Two Sorted Arrays
+    int index1 = 0, index2 = 0; 
+    maxArrayIndex = start_ind;
+
+    while (index1 < len1 && index2 < len2){
+        if (first[index1] < second[index2]){
+            array[maxArrayIndex++] = first[index1++];
         }
-
-        else{
-            cout << b[j++] <<" ";
+        else {
+            array[maxArrayIndex++] = second[index2++];
         }
     }
 
-    while(i<m){
-        cout << a[i++] <<" ";
+    while (index1 < len1){
+        array[maxArrayIndex++] = first[index1++];
     }
 
-    while(j<n){
-        cout << b[j++] <<" ";
+    while (index2 < len2){
+        array[maxArrayIndex++] = second[index2++];
     }
-    
+
+    delete []first;
+    delete []second;
+}
+
+
+void mergeSort(int *array, int start_ind, int end_ind){
+
+    //pehle base case likhna ha
+    if (start_ind >= end_ind){
+        return;
+    }
+
+    int mid_ind = (start_ind + end_ind) / 2;
+
+    //left part sort krna ha
+    mergeSort(array, start_ind, mid_ind);
+
+    //right part sort krna hai
+    mergeSort(array, mid_ind+1, end_ind);
+
+    //Merge Arrays
+    merge(array, start_ind, end_ind);
+
 }
 
 int main(){
     
-    int a[] = {2,5,12,34};
-    int b[] = {1,3,4,43};
-    int m = sizeof(a)/sizeof(a[0]);
-	int n = sizeof(b)/sizeof(b[0]);
-    merge(a,b,m,n);
+    int array[] = {2,5,12,34,12,2,33,44,5,5,5,432,2,2,2,2};
+    
+    int n = 16;
+
+    mergeSort(array, 0, n-1);
+
+    for (int i=0; i<n; i++){
+        cout << array[i] <<" ";
+    }
+    cout << endl;
 
 }
