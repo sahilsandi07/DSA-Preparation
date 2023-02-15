1de776386506a8998a34f05b6d93e14ed40b6083
Sahil Sandi <officialsahilsandi@gmail.com>
2023-02-15 18:05:44 +0530

    I have Solved the Question Merge Two Sorted Arrays with Efficient Method, I think this is newest Algorithm.

--- Sorting/mergeTwoSortedArrayEfficientMethod.cpp
+++ Sorting/mergeTwoSortedArrayEfficientMethod.cpp
@@ -1,25 +1,33 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+//Time Complexity is O((n+m)log(n+m))
+//The Algorithm is Written by me And I have alll rights of it.
+
 void merge(int arr1[], int arr2[], int n, int m){
     vector<int> merged(n+m);
-        
+
+    //copy elements of array 1 to vector merged for position 0 t0 n-1 
     for (int i=0; i<n; i++){
         merged[i] = arr1[i];
     }
-        
+
+    //copy elements of array 2 to vector merged for position n-1 t0 m
     int j=n;
     for (int i=0; i<m; i++){
         merged[j] = arr2[i];
         j++;
     }
-        
+
+    //sort the merged vector    
     sort(merged.begin(),merged.end());
-        
+
+    //copy the sorted values from merged vector to array 1    
     for(int i=0; i<n; i++){
         arr1[i] = merged[i];
     }
-        
+
+    //copy the sorted values from merged vector to array 2    
     int k = n;
     for (int i=0; i<m; i++){
         arr2[i] = merged[k];
