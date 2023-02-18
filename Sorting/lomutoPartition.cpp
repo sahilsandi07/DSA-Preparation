627c1763a7ab58ff91c722485caf20acf1ad0699
Sahil Sandi <officialsahilsandi@gmail.com>
2023-02-18 12:18:55 +0530

    Lomuto Partition

--- Sorting/lomutoPartition.cpp
+++ Sorting/lomutoPartition.cpp
@@ -3,24 +3,26 @@ using namespace std;
 
 void lomuto_partition(int arr[], int left_index, int right_index){
 
+    // select the pivot element as  last element 
+    // if pivot element is user input swap the pivot element with last element
     int pivot_element = arr[right_index];
 
+    // initialise the iterator
     int iterator = left_index - 1;
 
+    //start traversing from 0 to right_index - 1;
     for (int j = left_index; j <= right_index-1; j ++){
 
-        if (arr[j] < pivot_element){
+        if (arr[j] < pivot_element){        // if j'th element is smaller than pivot
 
-            iterator++;
-            swap(arr[iterator], arr[j]);
+            iterator++;                     //increamnet the iterator 
+            swap(arr[iterator], arr[j]);    //swap arr[iterator] with arr[j]
 
         }
 
     }
 
-    swap (arr[iterator+1], arr[right_index]);
-
-    //cout << arr[pivot_index] << endl;
+    swap (arr[iterator+1], arr[right_index]);       //when traversing is finished swap arr[iterator+1] with arr[right_index]
     
     for (int i = 0; i < right_index+1; i++){
         cout << arr[i] <<" ";
@@ -33,8 +35,6 @@ void lomuto_partition(int arr[], int left_index, int right_index){
             break;
         }
     }
-
-    //return iterator+1;
 }
 
 int main(){
