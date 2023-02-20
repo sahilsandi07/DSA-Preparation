9b37600cdbe3409122b4ad6f855bcd4ecd541bb7
Sahil Sandi <officialsahilsandi@gmail.com>
2023-02-20 15:07:46 +0530

    Minimum Platforms Required for safe Arrival of Trains

---
+++ Sorting/minmumPlatforms.cpp
@@ -0,0 +1,28 @@
+class Solution{
+    public:
+    //Function to find the minimum number of platforms required at the
+    //railway station such that no train waits.
+    int findPlatform(int arr[], int dep[], int n)
+    {
+    	sort(arr, arr+n);
+    	sort(dep, dep+n);
+    	
+    	int i = 1, j = 0, res = 1, curr = 1;
+    	
+    	while (i<n && j<n){
+    	    if (arr[i] <= dep[j]){
+    	        curr++;
+    	        i++;
+    	    }
+    	    
+    	    else {
+    	        curr--;
+    	        j++;
+    	    }
+    	    
+    	    res = max(res, curr);
+    	}
+    	
+    	return res;
+    }
+};
