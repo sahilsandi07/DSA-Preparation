40eba490345a2343afcfc973903c6149dcf8f290
Sahil Sandi <officialsahilsandi@gmail.com>
2023-04-07 11:14:41 +0530

    New Approach T.C O(n), S.C O(n)

--- LinkedList/cloneLL.cpp
+++ LinkedList/cloneLL.cpp
@@ -1,79 +1,64 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-struct Node
+class Solution
 {
-    int data;
-    Node *next, *random;
-    Node(int x)
+    private:
+    void insertAtTail (Node* &head, Node* &tail, int d)
     {
-        data = x;
-        next = random = NULL;
+        Node* newNode = new Node (d);
+        
+        if (head == NULL)
+        {
+            head = newNode;
+            tail = newNode;
+            return;
+        }
+        else
+        {
+            tail->next = newNode;
+            tail = newNode;
+        }
     }
+    public:
+    Node *copyList(Node *head)
+    {
+        //Step 1: Create a Clone
+        Node* cloneHead = NULL;
+        Node* cloneTail = NULL;
+        
+        Node* temp = head;
+        
+        while (temp != NULL)
+        {
+            insertAtTail (cloneHead, cloneTail, temp->data);
+            temp = temp -> next;
+        }
+        
+        //Step 2: create a map
+        unordered_map<Node*, Node*> oldToNewNode;
+        
+        Node* originalNode = head;
+        Node* cloneNode = cloneHead;
+        
+        while (originalNode != NULL && cloneNode != NULL)
+        {
+            oldToNewNode[originalNode] = cloneNode;
+            originalNode = originalNode->next;
+            cloneNode = cloneNode->next;
+        }
+        
+        originalNode = head;
+        cloneNode = cloneHead;
+        
+        while (originalNode != NULL && cloneNode != NULL)
+        {
+            cloneNode->arb = oldToNewNode[originalNode->arb];
+            cloneNode = cloneNode->next;
+            originalNode = originalNode->next;
+        }
+        
+        return cloneHead;
+    }
+
 };
-
-void print(Node *start)
-{
-    Node *ptr = start;
-    while (ptr)
-    {
-        cout << "Data = " << ptr->data << ", Random  = " << ptr->random->data << endl;
-        ptr = ptr->next;
-    }
-}
-
-Node *clone(Node *head)
-{
-    Node *next, *temp;
-    for (Node *curr = head; curr != NULL;)
-    {
-        next = curr->next;
-        curr->next = new Node(curr->data);
-        curr->next->next = next;
-        curr = next;
-    }
-    for (Node *curr = head; curr != NULL; curr = curr->next->next)
-    {
-        curr->next->random = (curr->random != NULL) ? (curr->random->next) : NULL;
-    }
-
-    Node *original = head, *copy = head->next;
-
-    temp = copy;
-
-    while (original && copy)
-    {
-        original->next =
-            original->next ? original->next->next : original->next;
-
-        copy->next = copy->next ? copy->next->next : copy->next;
-        original = original->next;
-        copy = copy->next;
-    }
-
-    return temp;
-}
-
-int main()
-{
-    Node *head = new Node(10);
-    head->next = new Node(5);
-    head->next->next = new Node(20);
-    head->next->next->next = new Node(15);
-    head->next->next->next->next = new Node(20);
-
-    head->random = head->next->next;
-    head->next->random = head->next->next->next;
-    head->next->next->random = head;
-    head->next->next->next->random = head->next->next;
-    head->next->next->next->next->random = head->next->next->next;
-
-    cout << "Original list : \n";
-    print(head);
-
-    cout << "\nCloned list : \n";
-    Node *cloned_list = clone(head);
-    print(cloned_list);
-
-    return 0;
-}
