bb3d0319cfa1c70fcf1c1dd9ab5a6ea5340b07b1
Sahil Sandi <officialsahilsandi@gmail.com>
2023-04-09 10:57:08 +0530

    Stack Operations

---
+++ Stack/stack_opr.cpp
@@ -0,0 +1,5 @@
+// isEmpty () : it retuens true if stack is Empty
+// push (x) : Insert Element at top
+// pop () : removes element from top
+// peek () : returns the top element
+// size() : returns the size of the stack
