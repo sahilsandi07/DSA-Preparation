8543c55c1c3d2ed912feaaef82cbe9b0f84b9e1b
Sahil Sandi <officialsahilsandi@gmail.com>
2023-04-24 10:04:39 +0530

    Operations on Dequeue

---
+++ Dequeue/DequeueOperations.cpp
@@ -0,0 +1,13 @@
+/*
+void eraseAt(deque <int> &deq, int X){
+    deq.erase(deq.begin() + X);
+}
+
+void eraseInRange(deque<int> &deq, int start, int end){
+    deq.erase(deq.begin() + start,deq.begin() + end);
+}
+
+void eraseAll(deque<int> &deq){
+   deq.clear();
+}
+*/
