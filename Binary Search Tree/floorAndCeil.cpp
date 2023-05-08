77c492edea938727438cd42754e16599d42209a2
Sahil Sandi <officialsahilsandi@gmail.com>
2023-05-08 10:49:02 +0530

    Floor and Ceil of a key in the BST.

--- Binary Search Tree/floorAndCeil.cpp
+++ Binary Search Tree/floorAndCeil.cpp
@@ -4,12 +4,14 @@
 #include <queue>
 using namespace std;
 
+// Node structure for BST
 struct Node {
     int data;
     Node* left;
     Node* right;
 };
 
+// Function to create a new node with given key
 Node* newNode(int key) {
     Node* node = new Node;
     node->data = key;
@@ -18,58 +20,39 @@ Node* newNode(int key) {
     return node;
 }
 
+// Function to find the floor of given key in BST
 int floor(Node* root, int key) {
-    vector<int> levelOrder;
+    Node* res = NULL;
     
-    if (root == NULL)
-    {
-        return -1;
-    }
-    
-    queue<Node*> q;
-    q.push(root);
-    
-    while (!q.empty())
-    {
-        Node* curr = q.front();
-        q.pop();
-        
-        levelOrder.push_back(curr -> data);
-        
-        if (curr -> left)
-        {
-            q.push(curr -> left);
-        }
-        if (curr -> right)
-        {
-            q.push(curr -> right);
+    while (root != NULL) {
+        if (root -> data == key) {
+            // If the current node has the key value, return its data value
+            return root -> data;
+        } else if (root -> data > key) {
+            // If the current node's data value is greater than the key value,
+            // move to its left child
+            root = root -> left;
+        } else {
+            // If the current node's data value is less than the key value,
+            // update the result to the current node and move to its right child
+            res = root;
+            root = root -> right;
         }
     }
     
-    sort (levelOrder.begin(), levelOrder.end());
-    
-    for (int i=0; i<levelOrder.size(); i++)
-    {
-        if (levelOrder[i] == key)
-        {
-            return levelOrder[i];
-        }
-        else if (levelOrder[i] > key)
-        {
-            return levelOrder[i-1];
-        }
-        else
-        {
-            return -1;
-        }
-    }
+    // If we reach a leaf node without finding the key value, return the
+    // data value of the last node visited with a data value less than the key value
+    return res != NULL ? res->data : -1;
 }
 
+// Function to find the ceiling of given key in BST
 int findCeil(Node* root, int input) {
+    // If tree is empty, return -1
     if (root == NULL) return -1;
     
     Node* res = NULL;
     
+    // Traverse the tree to find the ceiling
     while (root != NULL)
     {
         if (root -> data == input)
@@ -92,7 +75,9 @@ int findCeil(Node* root, int input) {
     return root != NULL ? res -> data : -1;
 }
 
+// Main function to test the code
 int main() {
+    // Create a sample BST
     Node* root = newNode(8);
     root->left = newNode(4);
     root->right = newNode(12);
@@ -101,6 +86,7 @@ int main() {
     root->right->left = newNode(10);
     root->right->right = newNode(14);
     
+    // Test the floor and ceil functions
     int key = 5;
     cout << "Floor of " << key << " is: " << floor(root, key) << endl;
     
