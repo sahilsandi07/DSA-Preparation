a66f618ff8cdbe446c5687408010a1bf1e2f7b48
Sahil Sandi <officialsahilsandi@gmail.com>
2023-06-07 09:56:51 +0530

    Prims Algorithm

---
+++ Graphs/primsAlgo.cpp
@@ -0,0 +1,74 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+	public:
+	//Function to find sum of weights of edges of the Minimum Spanning Tree.
+    int spanningTree(int V, vector<vector<int>> adj[])
+    {
+        // declare a priority queue or a min heap
+        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+        
+        //declare a visited array
+        vector<int> vis (V, 0);
+        
+        // pq --> {wt, node}
+        pq.push({0, 0});
+        
+        int sum = 0;
+        
+        // iterate till pq becomes empty
+        while (!pq.empty()) {
+            int wt = pq.top().first;
+            int node = pq.top().second;
+            pq.pop();
+            
+            //if node i already visited, go get the next node
+            if (vis[node]) continue;
+            // otherwise welcome it and mark it as visited
+            vis[node] = 1;
+            
+            sum += wt;
+            
+            for (auto it : adj[node]) {
+                int adjNode = it[0];
+                int edW = it[1];
+                
+                if (!vis[adjNode]) {
+                    pq.push({edW, adjNode});
+                }
+            }
+        }
+        
+        return sum;
+    }
+};
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--) {
+        int V, E;
+        cin >> V >> E;
+        vector<vector<int>> adj[V];
+        int i=0;
+        while (i++<E) {
+            int u, v, w;
+            cin >> u >> v >> w;
+            vector<int> t1,t2;
+            t1.push_back(v);
+            t1.push_back(w);
+            adj[u].push_back(t1);
+            t2.push_back(u);
+            t2.push_back(w);
+            adj[v].push_back(t2);
+        }
+        
+        Solution obj;
+    	cout << obj.spanningTree(V, adj) << "\n";
+    }
+
+    return 0;
+}
