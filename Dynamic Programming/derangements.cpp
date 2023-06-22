6a98c774a4be0778be5b80c7167fdb816e862ce1
Sahil Sandi <officialsahilsandi@gmail.com>
2023-06-22 10:38:52 +0530

    Derangements

--- Dynamic Programming/derangements.cpp
+++ Dynamic Programming/derangements.cpp
@@ -37,6 +37,24 @@ long long int solveTab(int n) {
     return dp[n];
 }
 
+long long int solveTabSO(int n) {
+    // base case
+    long long int prev2 = 0;
+    long long int prev1 = 1;
+
+    for (int i=3; i<=n; i++) {
+        long long int first = prev1 % MOD;
+        long long int second = prev2 % MOD;
+
+        long long int ans = ((i-1)*(first + second)) % MOD;
+
+        prev2 = prev1;
+        prev1 = ans;
+    }
+
+    return prev1;
+}
+
 long long int countDerangements(int n) {
     // 1 -> Recursion
     // return solveRec(n);
@@ -46,5 +64,8 @@ long long int countDerangements(int n) {
     // return solveMem(n, dp);
 
     // 3 -> Tabularisation
-    return solveTab(n);
+    // return solveTab(n);
+
+    // 4 -> Tabularisation with SO
+    return solveTabSO (n);
 }
