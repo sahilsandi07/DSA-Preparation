5a48ac145fb516a5da950302a85b955c13890f81
Sahil Sandi <officialsahilsandi@gmail.com>
2023-07-26 11:44:42 +0530

    Added Tabulation Method

--- Dynamic Programming/longestCommonSubSequence.cpp
+++ Dynamic Programming/longestCommonSubSequence.cpp
@@ -30,6 +30,27 @@ private:
         return dp[n][m] = max (solveMem(n-1, m, s1, s2, dp), solveMem(n, m-1, s1, s2, dp));        
     }
 
+    int solveTab(string text1, string text2) {
+        int n = text1.size();
+        int m = text2.size();
+
+        vector<vector<int>> dp(n + 1, vector<int>(m + 1, 0));
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                // if the characters match
+                if (text1[i - 1] == text2[j - 1]) {
+                    dp[i][j] = 1 + dp[i - 1][j - 1];
+                } else {
+                    dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+
+        return dp[n][m];
+    }
+
+
 public:
     int longestCommonSubsequence(string text1, string text2) {
         // 1 -> Recursion
