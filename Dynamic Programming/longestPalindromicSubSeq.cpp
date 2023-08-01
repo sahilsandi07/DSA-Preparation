ce5d0ac9e6b56beb1db876d48828abc3ae91d899
Sahil Sandi <officialsahilsandi@gmail.com>
2023-08-01 20:06:41 +0530

    Longest Palindromic SubSequence

---
+++ Dynamic Programming/longestPalindromicSubSeq.cpp
@@ -0,0 +1,29 @@
+class Solution {
+private:
+    int longestCommonSubsequence(string text1, string text2) {
+        int n = text1.size();
+        int m = text2.size();
+
+        vector<vector<int>> dp(n + 1, vector<int>(m + 1, 0));
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                // if the characters match
+                if (text1[i - 1] == text2[j - 1]) {
+                    dp[i][j] = 1 + dp[i - 1][j - 1];
+                } else {
+                    dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+
+        return dp[n][m];
+    }
+
+public:
+    int longestPalindromeSubseq(string s) {
+        string t = s;
+        reverse(t.begin(), t.end());
+        return longestCommonSubsequence (s, t);
+    }
+};
